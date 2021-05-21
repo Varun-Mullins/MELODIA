7d520d3ddaee23d42ea3531ed6235824b5697f1d
Varun-Mullins <57276726+Varun-Mullins@users.noreply.github.com>
2021-05-21 12:38:01 -0400

    Add files via upload

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import LoginNavbar from './components/LoginNavbar';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/pages/Login';
 import Home from './components/pages/Home';
 import AboutUs from './components/pages/AboutUs';
+import AboutUsHome from './components/pages/AboutUsHome';
 import Playlist from './components/pages/Playlist';
 import Recents from './components/pages/Recents';
 import Tracks from './components/pages/Tracks';
-
+import UserPlaylists from './components/pages/UserPlaylists';
+import Recommendations from './components/pages/Recommendations';
 
 function App() {
   return (
@@ -18,10 +19,13 @@ function App() {
           <Route path='/' exact component={Login} />
           <Route path='/home' exact component={Home} />
           <Route path='/aboutus' exact component={AboutUs} />
+          <Route path='/aboutus_home' exact component={AboutUsHome} />
           <Route path='/playlist' exact component={Playlist} />
           <Route path='/recents' exact component={Recents} />
           <Route path='/tracks' exact component={Tracks} />
-
+          <Route path='/showplaylists' exact component={UserPlaylists} />
+          <Route path='/recommendations' exact component={Recommendations} />
+          <Route path='/recenttracks' exact component={Recents} />
         </Switch>
       </Router>
 
